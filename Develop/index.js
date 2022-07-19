c96a63dae4478d75acc2a315c523d273a37c5ce5
KappaMustafa <steef136@gmail.com>
2022-07-19 02:55:43 -0500

    added more to index.js

--- Develop/index.js
+++ Develop/index.js
@@ -1,23 +1,24 @@
 // TODO: Include packages needed for this application
-const fs = resquire('fs')
-const inquirer = require('inquirer')
+const fs = require('fs')
+const inquirer = require('inquirer');
+const Choices = require('inquirer/lib/objects/choices');
 // TODO: Create an array of questions for user input
 const questions = [
     {
     type: 'input',
-    name: 'file name',
+    name: 'name',
     message: 'whats the name of the md file you want to create?'
 
     },
     {
         type: 'input',
-        name: 'file description',
+        name: 'description',
         message: 'describe your program?'
     
     },
     {
         type: 'input',
-        name: 'how to isntall',
+        name: 'installation',
         message: 'howm to install your program?'
     
     },
@@ -39,13 +40,34 @@ const questions = [
         message: 'how to test this program?'
     
     },
-];
+    {
+        type: 'list',
+        name: 'license',
+        message: 'select a license thing',
+        Choices: [
 
+        ]
+    },
+];
 // TODO: Create a function to write README file
-function writeToFile(README, answers) {}
+function writeToFile(answers) {
+    fs.writeFile('readme.md', 
+    `
+    ${name}
+    ${description}
+    ${installation}
+    ${usage}
+    ${guidelines}
+    ${testing}
+    ${license}
+    `)
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {inquirer.prompt(questions).then((answers) => {
+    console.log(answers);
+}
+)}
 
 // Function call to initialize app
 init();
